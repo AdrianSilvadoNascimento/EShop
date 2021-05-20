deb4abb9e0dca9ba5a73e426ddecc9cc75e5fb84
Adrian Silva <adrianfsf10@gmail.com>
2021-05-19 21:38:46 -0300

    Alterado Linha de categorias.

--- scriptsJs/produtos.js
+++ scriptsJs/produtos.js
@@ -1,10 +1,17 @@
 const blusasVitrine = [
-    {id: 0, nome: 'Blusa-Branca', img: '../images/camisa-branca.jpg', quantidade: 0, preco: 'R$34,99', estoque: '38'},
-    {id: 1, nome: 'Blusa-Branca-Mulher', img: '../images/camisa-branca-mulher.jpg', quantidade: 0, preco: 'R$29,99' , estoque: '24'},
-    {id: 2, nome: 'Blusa-Preta', img: '../images/camisa-preta.jpg', quantidade: 0, preco: 'R$34,99', estoque: '26'},
-    {id: 3, nome: 'Blusa-Preta-Mulher', img: '../images/camisa-preta-mulher.jpg', quantidade: 0, preco: 'R$29,99', estoque: '31'},
-    {id: 4, nome: 'Livro-Azul', img: '../images/livro1.jpg', quantidade: 0, preco: 'R$21,99', estoque: '55'},
-    {id: 5, nome: 'Livro-Rosa', img: '../images/livro2.jpg', quantidade: 0, preco: 'R$21,99', estoque: '65'},
+    {id: 0, nome: 'Blusa-Branca', img: '../images/camisa-branca.jpg', quantidade: 0, categoria: 'camisa', preco: 'R$34,99', estoque: '38'},
+    {id: 1, nome: 'Blusa-Branca-Mulher', img: '../images/camisa-branca-mulher.jpg', quantidade: 0, categoria: 'camisa', preco: 'R$29,99' , estoque: '24'},
+    {id: 2, nome: 'Blusa-Preta', img: '../images/camisa-preta.jpg', quantidade: 0, categoria: 'camisa', preco: 'R$34,99', estoque: '26'},
+    {id: 3, nome: 'Blusa-Preta-Mulher', img: '../images/camisa-preta-mulher.jpg', quantidade: 0, categoria: 'camisa', preco: 'R$29,99', estoque: '31'},
+    {id: 4, nome: 'Blusa-Branca', img: '../images/camisa-branca.jpg', quantidade: 0, categoria: 'camisa', preco: 'R$34,99', estoque: '38'},
+    {id: 5, nome: 'Blusa-Branca-Mulher', img: '../images/camisa-branca-mulher.jpg', quantidade: 0, categoria: 'camisa', preco: 'R$29,99' , estoque: '24'},
+    {id: 6, nome: 'Blusa-Preta', img: '../images/camisa-preta.jpg', quantidade: 0, categoria: 'camisa', preco: 'R$34,99', estoque: '26'},
+    {id: 7, nome: 'Blusa-Preta-Mulher', img: '../images/camisa-preta-mulher.jpg', quantidade: 0, categoria: 'camisa', preco: 'R$29,99', estoque: '31'},
+    {id: 8, nome: 'Livro-Azul', img: '../images/livro1.jpg', quantidade: 0, categoria: 'livro', preco: 'R$21,99', estoque: '55'},
+    {id: 9, nome: 'Livro-Rosa', img: '../images/livro2.jpg', quantidade: 0, categoria: 'livro', preco: 'R$21,99', estoque: '65'},
+    {id: 10, nome: 'Livro-Azul', img: '../images/livro1.jpg', quantidade: 0, categoria: 'livro', preco: 'R$21,99', estoque: '65'},
+    {id: 11, nome: 'Livro-Rosa', img: '../images/livro2.jpg', quantidade: 0, categoria: 'livro', preco: 'R$21,99', estoque: '65'},
+    {id: 12, nome: 'Livro-Azul', img: '../images/livro1.jpg', quantidade: 0, categoria: 'livro', preco: 'R$21,99', estoque: '65'},
 ]
 
 iniciarLoja = () => {
@@ -13,7 +20,7 @@ iniciarLoja = () => {
     //produtos.map((val)=>{
 
         conteudoProdutos.innerHTML += `
-        <div class="card">
+        <div class="card" id="` + blusasVitrine[i].categoria + `">
             <img src="`+ blusasVitrine[i].img +`" class="img-fluid" alt="camisa">
             <div class="row">
                 <div class="col-md-6">
