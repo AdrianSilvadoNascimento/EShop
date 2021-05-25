3dd1a8c13a4ca344a95bed3e375e383096a14840
Adrian Silva <adrianfsf10@gmail.com>
2021-05-25 11:50:55 -0300

    HomePage responsiva.

---
+++ scriptsJs/menu-drop.js
@@ -0,0 +1,9 @@
+let buttonDrop = document.getElementById('menuSmall');
+buttonDrop.style.display = 'none';
+
+function abrirMenuDrop(){
+    buttonDrop.style.display = 'block';
+}
+function fecharMenuDrop(){
+    buttonDrop.style.display = 'none';
+}
