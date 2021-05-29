e10105cb07232979798cc44740d5dce5c6c49fe9
Adrian Silva <adrianfsf10@gmail.com>
2021-05-28 23:17:00 -0300

    Search funciona. botão search não funciona. erro ao computar quantidade de produto ao clicar em adicionar.

--- app/angular/modules/lojaVirtual.js
+++ app/angular/modules/lojaVirtual.js
@@ -1,7 +1,7 @@
 angular.module("lojaVirtual").controller("lojaVirtualCtrl", function($scope){
     $scope.logo="Loja Virtual"
 
-    $scope.blusas = [
+    $scope.produtosLoja = [
         {id: 0, nome: 'camisasaBranca', img: '../images/camisa-branca.jpg', quantidade: 0, preco: 'R$34,99', estoque: '38'},
         {id: 1, nome: 'camisaBrancaMulher', img: '../images/camisa-branca-mulher.jpg', quantidade: 0, preco: 'R$29,99' , estoque: '24'},
         {id: 2, nome: 'camisaPreta', img: '../images/camisa-preta.jpg', quantidade: 0, preco: 'R$34,99', estoque: '26'},
@@ -9,13 +9,21 @@ angular.module("lojaVirtual").controller("lojaVirtualCtrl", function($scope){
         {id: 4, nome: 'camisaBranca', img: '../images/camisa-branca.jpg', quantidade: 0, preco: 'R$34,99', estoque: '38'},
         {id: 5, nome: 'camisaBrancaMulher', img: '../images/camisa-branca-mulher.jpg', quantidade: 0, preco: 'R$29,99' , estoque: '24'},
         {id: 6, nome: 'camisaPreta', img: '../images/camisa-preta.jpg', quantidade: 0, preco: 'R$34,99', estoque: '26'},
-        {id: 7, nome: 'camisaPretaMulher', img: '../images/camisa-preta-mulher.jpg', quantidade: 0, preco: 'R$29,99', estoque: '31'}
+        {id: 7, nome: 'camisaPretaMulher', img: '../images/camisa-preta-mulher.jpg', quantidade: 0, preco: 'R$29,99', estoque: '31'},
+        {id: 8, nome: 'livroRosa', img: '../images/livro2.jpg', quantidade: 0, preco: 'R$29,99', estoque: '31'},
+        {id: 9, nome: 'livroAzul', img: '../images/livro1.jpg', quantidade: 0, preco: 'R$29,99', estoque: '31'},
+        {id: 10, nome: 'livroRosa', img: '../images/livro2.jpg', quantidade: 0, preco: 'R$29,99', estoque: '31'}
     ];
 
-    $scope.adicionarNaLista = function(blusa){
-        $scope.blusas.push(angular.copy(blusa));
-        delete $scope.blusa;
-        console.log($scope.blusas);
+    $scope.categorias = [
+        {nome: 'camisa'},
+        {nome: 'livro'}
+    ];
+
+    $scope.adicionarNaLista = function(produto){
+        $scope.produtosLoja.push(angular.copy(produto));
+        delete $scope.produto;
+        console.log($scope.produtosLoja);
     }
         
 });
