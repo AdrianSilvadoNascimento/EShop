d9b4d470d69c96f1b1a595b7f6ac264296803eb0
Adrian Silva <adrianfsf10@gmail.com>
2021-10-03 08:59:12 -0300

    Adiciona correções ao card que dropa

--- scriptsJs/abrir-bag-drop.js
+++ scriptsJs/abrir-bag-drop.js
@@ -2,27 +2,36 @@
 var abrir = document.getElementById('abrir-drop'),
     fechar = document.getElementById('fechar-drop'),
     bag = document.getElementById('bag');
+
+function showHide(){
+    if(bag.style.display="none"){
+        bag.style.display="show";
+    }else if(bag.style.display="show"){
+        bag.style.display="none";
+    }
+}
 function addClass(){
-    bag.style.display='show';
     if(!hasClass()){
         setTimeout(function (){
             if(hasClass()){
-                bag.style.display='none';
+                showHide();
                 return bag.classList.remove('bag-drop');
             }
         },3000);
+        showHide();
         return bag.classList.add('bag-drop');
     }
 }
 function removeClass(){
     if(hasClass()){
-        bag.style.display='none';
+        showHide();
         return bag.classList.remove('bag-drop');
     }
 }
 function hasClass(){
     return bag.classList.contains('bag-drop');
 }
+showHide();
 abrir.addEventListener('click', addClass);
 fechar.addEventListener('click', removeClass);
 //Chamar bag-drop JS puro
